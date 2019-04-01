7bed4cef4745dca6e52cab83f68c458f52afa431
Glavak <gau-gau@ya.ru>
2019-04-01 09:37:48 +0700

    Basic hands

--- Assets/BeltsManager.cs
+++ Assets/BeltsManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,7 @@ public class BeltsManager : MonoBehaviour
     public Text TimeText;
 
     [HideInInspector] public List<BeltSystem> Belts = new List<BeltSystem>();
+    [HideInInspector] public List<Hand> Hands = new List<Hand>();
 
     private Camera mainCamera;
     private Stopwatch stopwatch = new Stopwatch();
@@ -24,27 +26,60 @@ public class BeltsManager : MonoBehaviour
         stopwatch.Restart();
         Bounds cameraBounds = new Bounds((Vector2)mainCamera.transform.position, new Vector3(20, 10));
 
-        for (int i = 0; i < Belts.Count; i++)
+        foreach (BeltSystem beltSystem in Belts)
         {
-            for (int j = 0; j < Belts[i].Items.Count; j++)
+            for (int j = 0; j < beltSystem.Items.Count; j++)
             {
-                var items = Belts[i].Items;
+                var items = beltSystem.Items;
 
                 float progress = items[j].Progress + Time.deltaTime;
-                if (progress > Belts[i].Points.Length - 1) progress = Belts[i].Points.Length - 1.01f;
+                if (progress > beltSystem.Points.Length - 1.125f) progress = beltSystem.Points.Length - 1.125f;
                 
                 items[j] = new ItemOnBelt(items[j].Item, progress);
             }
 
-            if (cameraBounds.Intersects(Belts[i].Bounds))
+            if (cameraBounds.Intersects(beltSystem.Bounds))
             {
-                for (int j = 0; j < Belts[i].Items.Count; j++)
+                for (int j = 0; j < beltSystem.Items.Count; j++)
                 {
-                    float itemProgr = Belts[i].Items[j].Progress;
-                    Vector2 a = Belts[i].Points[(int) itemProgr];
-                    Vector2 b = Belts[i].Points[(int) itemProgr + 1];
+                    float itemProgr = beltSystem.Items[j].Progress;
+                    Vector2 a = beltSystem.Points[(int) itemProgr];
+                    Vector2 b = beltSystem.Points[(int) itemProgr + 1];
                     float p = itemProgr - (int) itemProgr;
-                    Belts[i].Items[j].Item.transform.localPosition = Vector2.Lerp(a, b, p);
+                    beltSystem.Items[j].Item.transform.localPosition = Vector2.Lerp(a, b, p);
+                }
+            }
+        }
+
+        foreach (Hand hand in Hands)
+        {
+            if (hand.ItemOnBelt.HasValue)
+            {
+                var item = hand.ItemOnBelt.Value; 
+                float progress = item.Progress + Time.deltaTime;
+                if (progress >= 1)
+                {
+                    item.Progress = hand.ToProgress;
+                    hand.To.Items.Add(item);
+                    hand.ItemOnBelt = null;
+                }
+                else
+                {
+                    item.Progress = progress;
+                    hand.ItemOnBelt = item;
+                }
+                
+                hand.Sprite.transform.localRotation = Quaternion.Euler(0,0,progress*180);
+                item.Item.transform.localPosition = hand.Sprite.transform.GetChild(0).position; // TODO: might be slow
+            }
+            else
+            {
+                var item = PopClose(hand.From, hand.FromProgress);
+                if (item.HasValue)
+                {
+                    ItemOnBelt itemValue = item.Value;
+                    itemValue.Progress = 0;
+                    hand.ItemOnBelt = itemValue;
                 }
             }
         }
@@ -56,6 +91,23 @@ public class BeltsManager : MonoBehaviour
         
         stopwatch.Stop();
         TimeText.text = stopwatch.ElapsedMilliseconds + " ms / frame";
+    }
+
+    private ItemOnBelt? PopClose(BeltSystem beltSystem, float targetProgress)
+    {
+        // TODO: very slow
+
+        for (int i = 0; i < beltSystem.Items.Count; i++)
+        {
+            if (Mathf.Abs(beltSystem.Items[i].Progress - targetProgress) < 0.125f)
+            {
+                ItemOnBelt result = beltSystem.Items[i];
+                beltSystem.Items.RemoveAt(i);
+                return result;
+            }
+        }
+
+        return null;
     }
 
     public void SpawnItem(Vector2 worldPosition)
@@ -80,24 +132,27 @@ public class BeltsManager : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
+        if (mainCamera == null) return;
+        
         foreach (BeltSystem beltSystem in Belts)
         {
             Gizmos.color = new Color(1, 0, .1f, 0.2f);
             Gizmos.DrawCube(transform.position + beltSystem.Bounds.center, beltSystem.Bounds.size);
         }
+
         Bounds cameraBounds = new Bounds(mainCamera.transform.position, new Vector3(20, 10, 10));
         Gizmos.color = new Color(1, 0, .5f, 0.2f);
         Gizmos.DrawCube(transform.position + cameraBounds.center, cameraBounds.size);
     }
 }
 
-public struct BeltSystem
+public class BeltSystem
 {
     public Vector2[] Points;
     public List<ItemOnBelt> Items;
     public Bounds Bounds;
 
-    public BeltSystem(Vector2[] points) : this()
+    public BeltSystem(Vector2[] points)
     {
         Points = points;
         Items = new List<ItemOnBelt>();
@@ -109,6 +164,18 @@ public struct BeltSystem
 
         Bounds.Expand(0.5f);
     }
+}
+
+public class Hand
+{
+    public BeltSystem From;
+    public float FromProgress;
+    
+    public BeltSystem To;
+    public float ToProgress;
+
+    public ItemOnBelt? ItemOnBelt;
+    public GameObject Sprite;
 }
 
 public struct ItemOnBelt
--- Assets/BeltsSpawner.cs
+++ Assets/BeltsSpawner.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class BeltsSpawner : MonoBehaviour
 {
     public BeltsManager Manager;
-    public GameObject BeltSprite;
+    [FormerlySerializedAs("BeltSprite")] public SpriteRenderer BeltSpritePrefab;
+    public GameObject HandPrefab;
     public Text CountText;
 
     public int RowsCount = 10;
@@ -16,26 +18,47 @@ public class BeltsSpawner : MonoBehaviour
     {
         for (int i = 0; i < RowsCount; i++)
         {
-            SpawnBelt(Vector2.right * 2 * i);
+            SpawnBelt(Vector2.right * 2 * i, i % 2 != 0);
+        }
+
+        for (int i = 0; i < RowsCount - 1; i++)
+        {
+            SpawnHand(i, i % 2 == 0);
         }
 
         CountText.text = RowsCount * BeltLength + " belts";
     }
 
-    private void SpawnBelt(Vector2 offset)
+    private void SpawnBelt(Vector2 offset, bool upside)
     {
         for (int i = 0; i < BeltLength; i++)
         {
-            Instantiate(BeltSprite, offset + new Vector2(0, -.5f - i), Quaternion.identity);
+            var belt = Instantiate(BeltSpritePrefab, offset + new Vector2(0, -.5f - i), Quaternion.identity);
+            belt.flipY = upside;
         }
 
         var points = new Vector2[BeltLength + 1];
         for (int i = 0; i < BeltLength + 1; i++)
         {
-            points[i] = offset + Vector2.down * i;
+            points[i] = offset + Vector2.down * (upside ? i : (BeltLength - i));
         }
 
         Manager.Belts.Add(new BeltSystem(points));
+    }
+
+    private void SpawnHand(int index, bool atUp)
+    {
+        var position = new Vector3(2 * index + 1, -(atUp ? 1 : BeltLength - 1));
+        GameObject go = Instantiate(HandPrefab, position, Quaternion.identity);
+        Hand hand = new Hand();
+
+        hand.Sprite = go.transform.GetChild(0).gameObject;
+        hand.From = Manager.Belts[index];
+        hand.To = Manager.Belts[index + 1];
+        hand.FromProgress = BeltLength - 1;
+        hand.ToProgress = 1;
+
+        Manager.Hands.Add(hand);
     }
 
     private void Update()
@@ -44,16 +67,12 @@ public class BeltsSpawner : MonoBehaviour
         {
             for (int x = 0; x < RowsCount; x++)
             {
-                for (int y = 0; y < 4; y++)
-                {
-                    Manager.SpawnItem(new Vector2(x * 2, y));
-                }
+                Manager.SpawnItem(new Vector2(x * 2, -4));
             }
-            
-            CountText.text += "," + RowsCount * 4 + " items";
+
+            CountText.text += "," + RowsCount + " items";
 
             firstUpdate = false;
         }
-
     }
 }
